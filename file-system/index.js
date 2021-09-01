de7cc6dfc1593d532b6fe77a74c0c87985036582
Tiago Santos <tiago881@gmail.com>
2021-09-01 19:20:58 -0300

    cria função que ler arquivos e diretórios

--- file-system/index.js
+++ file-system/index.js
@@ -1,6 +1,17 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-fs.readdir(path.resolve(__dirname))
-  .then((files) => console.log(files))
-  .catch((e) => console.log(e));
+async function readdir(rootDir) {
+  rootDir = rootDir || path.resolve(__dirname);
+  const files = await fs.readdir(rootDir);
+
+  walk(files);
+}
+
+function walk(files) {
+  for (const file of files) {
+    console.log(file);
+  }
+}
+
+readdir();
